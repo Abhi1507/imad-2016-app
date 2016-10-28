1b767ae2fc4f61e240ae995076fc9592ac5cb871
Abhi1507 <ahgandhi95@gmail.com>
2016-10-29 00:49:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('images/img1.gif', function (req, res) {
+app.get('/images/img1.gif', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui','images', 'img1.gif'));
 });
 
-app.get('images/img1.gif', function (req, res) {
+app.get('/images/img1.gif', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui','images', 'img2.gif'));
 });
 
