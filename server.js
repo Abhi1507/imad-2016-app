8a21bf74e4e60052a5e3e6601ffa09c38e10974b
Abhi1507 <ahgandhi95@gmail.com>
2016-10-29 00:49:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,12 +13,19 @@ app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('images/img1.gif', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','images', 'img1.gif'));
+});
+
+app.get('images/img1.gif', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui','images', 'img2.gif'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+var port = 8080; 
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
